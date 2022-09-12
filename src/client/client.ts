af563052ae4368c6d9ae6792d2bd30cb79d217c7
Bruno Dias <brunodg.dev@gmail.com>
2022-09-12 18:27:52 -0300

    feat: added camera values and an AxesHelper at the scene.

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,8 +1,10 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "dat.gui";
 
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -38,6 +40,34 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
+
+const gui = new GUI();
+
+const cubeFolder = gui.addFolder("Cube");
+
+const cubeRotationFolder = cubeFolder.addFolder("Rotation");
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+
+const cubePositionFolder = cubeFolder.addFolder("Position");
+cubePositionFolder.add(cube.position, "x", -10, 10);
+cubePositionFolder.add(cube.position, "y", -10, 10);
+cubePositionFolder.add(cube.position, "z", -10, 10);
+
+const cubeScaleFolder = cubeFolder.addFolder("Scale");
+cubeScaleFolder.add(cube.scale, "x", 0, 5);
+cubeScaleFolder.add(cube.scale, "y", 0, 5);
+cubeScaleFolder.add(cube.scale, "z", 0, 5);
+
+cubeFolder.open();
+cubeRotationFolder.open();
+cubePositionFolder.open();
+cubeScaleFolder.open();
+
+/*const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "z", 0, 20);
+cameraFolder.open();*/
 
 function animate() {
   requestAnimationFrame(animate);
